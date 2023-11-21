c72fbfb191e26e28fe37eef138f011564ae953be
irinag00 <irinagorlino@gmail.com>
2023-11-21 11:44:14 -0300

    añadi texto y un imagen

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola Coder!</Text>
+      <Text>Mi nombre es Irina Gorlino</Text>
+      <Text>¡Bienvenidos a mi tienda online!</Text>
+      <Image
+        style={{ width: 200, height: 200, marginTop: 30 }}
+        source={require("./assets/chica-selfie.png")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FF69B4",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
