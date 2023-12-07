3446b32bf06968daf356983a63207dd2aa4380d1
irinag00 <irinagorlino@gmail.com>
2023-12-06 21:01:50 -0300

    terminado las funcionalidades

--- App.js
+++ App.js
@@ -7,12 +7,15 @@ import {
   TextInput,
   Button,
   FlatList,
+  Modal,
 } from "react-native";
 import { useState } from "react";
 
 export default function App() {
   const [textInput, setTextInput] = useState("");
   const [itemList, setItemList] = useState([]);
+  const [itemSelectedToDelete, setItemSelectedToDelete] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
 
   const onChangeTextHandler = (text) => {
     setTextInput(text);
@@ -24,38 +27,79 @@ export default function App() {
     ]);
     setTextInput("");
   };
+  const onSelectItemHandler = (id) => {
+    setModalVisible(!modalVisible);
+    setItemSelectedToDelete(itemList.find((item) => item.id === id));
+  };
+  const onDeleteItemHandler = () => {
+    setItemList(itemList.filter((item) => item.id !== itemSelectedToDelete.id));
+    // setItemSelectedToDelete({});
+    setModalVisible(!modalVisible);
+  };
   const renderListItem = ({ item }) => (
     <View style={styles.itemList}>
       <Text>{item.value}</Text>
-      <Button color="#8A817C" title="X"></Button>
+      <Button
+        color="#463F3A"
+        title="X"
+        onPress={() => onSelectItemHandler(item.id)}
+      ></Button>
     </View>
   );
   return (
-    <View style={styles.container}>
-      <View style={styles.inputCointainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Ingresar tarea"
-          onChangeText={onChangeTextHandler}
-          value={textInput}
+    <>
+      <View style={styles.container}>
+        <View style={styles.containerText}>
+          <Text>¡Tu app de tareas!</Text>
+        </View>
+        <View style={styles.inputCointainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Ingresar tarea"
+            onChangeText={onChangeTextHandler}
+            value={textInput}
+          />
+          <Button title="Añadir" color="#E0AFA0" onPress={addItemToList} />
+        </View>
+        <FlatList
+          data={itemList}
+          renderItem={renderListItem}
+          keyExtractor={(item) => item.id}
         />
-        <Button title="Añadir" color="#E0AFA0" onPress={addItemToList} />
       </View>
-      <FlatList
-        data={itemList}
-        renderItem={renderListItem}
-        keyExtractor={(item) => item.id}
-      />
-    </View>
+      <Modal animationType="slide" visible={modalVisible}>
+        <View style={styles.modalMessageContainer}>
+          <Text>Se eliminará: </Text>
+          <Text>{itemSelectedToDelete.value}</Text>
+        </View>
+        <View style={styles.modalButtonContainer}>
+          <Button
+            color={"#BCB8B1"}
+            title="Cancelar"
+            onPress={() => setModalVisible(!modalVisible)}
+          ></Button>
+          <Button
+            color="#E0AFA0"
+            title="Eliminar"
+            onPress={() => onDeleteItemHandler()}
+          ></Button>
+        </View>
+      </Modal>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#BCB8B1",
+    backgroundColor: "#F4F3EE",
     alignItems: "start",
-    padding: 20,
+    padding: 50,
+  },
+  containerText: {
+    flexDirection: "column",
+    alignItems: "center",
+    marginBottom: 8,
   },
   inputCointainer: {
     flexDirection: "row",
@@ -73,7 +117,31 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 5,
     margin: 10,
-    backgroundColor: "#F4F3EE",
+    backgroundColor: "#BCB8B1",
     borderRadius: 10,
+  },
+  modalMessageContainer: {
+    marginTop: 50,
+    alignItems: "center",
+  },
+  modalButtonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    paddingTop: 20,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
